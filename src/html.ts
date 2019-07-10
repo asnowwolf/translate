201bba84534eec4b7110e944ac194f46c20c3c1b
Zhicheng WANG <zcwang@thoughtworks.com>
2019-07-10 16:11:50 +0800

    feat: 支持特殊标记 `[ng-should-translate]`，以便对其进行强制翻译

--- src/html.ts
+++ src/html.ts
@@ -2,7 +2,7 @@ import * as slugs from 'github-slugger';
 import { containsChinese } from './common';
 
 export namespace html {
-  export const defaultSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 't'];
+  export const defaultSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 't', '[ng-should-translate]'];
 
   export interface SentencePair {
     english: Element;
