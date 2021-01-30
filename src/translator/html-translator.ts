49b72a0dd76bd01d5f8c20faedd39f8dc5525d71
Zhicheng WANG <zcwang@thoughtworks.com>
2021-01-30 08:39:34 +0800

    refactor: html 翻译改成批量翻译

--- src/translator/html-translator.ts
+++ src/translator/html-translator.ts
@@ -19,30 +19,31 @@ export class HtmlTranslator extends Translator {
       .map(selector => Array.from(doc.querySelectorAll(selector)))
       .flat();
 
-    for (const element of elements) {
-      await this.translateElement(element);
-    }
+    const originals = elements.map(it => it.innerHTML);
+    const translations = await this.engine.translate(originals);
 
+    translations.forEach((translation, index) => {
+      applyTranslation(elements[index], translation);
+    });
     return doc;
   }
+}
 
-  private async translateElement(element: DomElement): Promise<string> {
-    if (shouldIgnore(element)) {
-      return element.innerHTML;
-    }
-    const translations = await this.engine.translate([element.innerHTML]);
-    const resultNode = new DomElement(element.tagName);
-    resultNode.innerHTML = translations[0];
-    element.parentNode.insertBefore(resultNode, element);
-    // 交换 id
-    const id = element.getAttribute('id');
-    if (id) {
-      resultNode.setAttribute('id', id);
-      element.removeAttribute('id');
-    }
-    resultNode.setAttribute('translation-result', 'on');
-    element.setAttribute('translation-origin', 'off');
+function applyTranslation(element: DomElement, translation: string): void {
+  if (shouldIgnore(element)) {
+    return;
   }
+  const resultNode = new DomElement(element.tagName);
+  resultNode.innerHTML = translation;
+  element.parentNode.insertBefore(resultNode, element);
+// 交换 id
+  const id = element.getAttribute('id');
+  if (id) {
+    resultNode.setAttribute('id', id);
+    element.removeAttribute('id');
+  }
+  resultNode.setAttribute('translation-result', 'on');
+  element.setAttribute('translation-origin', 'off');
 }
 
 function shouldIgnore(element: DomElement): boolean {
