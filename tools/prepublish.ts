1b6ffb4b3c47c41e8efeed06f9a49c822212e7cf
Zhicheng WANG <zcwang@thoughtworks.com>
2021-02-06 09:16:52 +0800

    fix: 增加预发布脚本，只发布 dist 目录下的内容

---
+++ tools/prepublish.ts
@@ -0,0 +1,8 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+const content = readFileSync(join(__dirname, '../package.json'), 'utf8');
+const json = JSON.parse(content);
+delete json.devDependencies;
+delete json.scripts;
+writeFileSync(join(__dirname, '../dist/package.json'), JSON.stringify(json, null, 2));
