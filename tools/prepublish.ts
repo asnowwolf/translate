768c8d8825b90c6a8cf49742de9f07c65c38253b
Zhicheng WANG <zcwang@thoughtworks.com>
2021-02-06 12:08:46 +0800

    fix: 把主包改为 private 的，以免无意中发布。

--- tools/prepublish.ts
+++ tools/prepublish.ts
@@ -5,4 +5,5 @@ const content = readFileSync(join(__dirname, '../package.json'), 'utf8');
 const json = JSON.parse(content);
 delete json.devDependencies;
 delete json.scripts;
+json.private = false;
 writeFileSync(join(__dirname, '../dist/package.json'), JSON.stringify(json, null, 2));
