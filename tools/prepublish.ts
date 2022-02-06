9dcc493d3857b020d717334397b8f4bfad96479f
Zhicheng WANG <zcwang@thoughtworks.com>
2022-02-06 23:35:25 +0800

    feat: 发布前为 `bin/index.js` 增加执行权限，以便本地测试

--- tools/prepublish.ts
+++ tools/prepublish.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { chmodSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
 const content = readFileSync(join(__dirname, '../package.json'), 'utf8');
@@ -7,3 +7,4 @@ delete json.devDependencies;
 delete json.scripts;
 json.private = false;
 writeFileSync(join(__dirname, '../dist/package.json'), JSON.stringify(json, null, 2));
+chmodSync(join(__dirname, '../dist/bin/index.js'), 0o755);
