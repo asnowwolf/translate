6565ffd598cc279f639a5c954f6415b38d64c37b
Zhicheng WANG <zcwang@thoughtworks.com>
2022-02-17 19:39:42 +0800

    feat: 强制还原文档 id 前面的行 fix: 强制还原文档头部的 include::

--- src/utils/asciidoctor/adoc-converter/renderers/document-renderer.ts
+++ src/utils/asciidoctor/adoc-converter/renderers/document-renderer.ts
@@ -1,6 +1,7 @@
 import * as asciidoctor from 'asciidoctor.js';
 import { BaseNodeRenderer } from './base-node-renderer';
 import { Author, DocumentNode } from './dom/models';
+import { restoreIncludeUrl } from './utils/restore-include-url';
 
 interface DocumentAttributes {
   doctitle: string;
@@ -11,6 +12,20 @@ function buildAuthorLine(author: Author) {
     return `${author.getName()} <${author.getEmail()}>`;
   } else {
     return author.getName();
+  }
+}
+
+function getLinesBeforeTitle(node: DocumentNode, id: string): string {
+  if (!id) {
+    return '';
+  }
+  const idText = `[[${id}]]`;
+  const lines = node.getSourceLines();
+  const index = lines.indexOf(idText);
+  if (index === -1) {
+    return idText;
+  } else {
+    return lines.slice(0, index + 1).join('\n');
   }
 }
 
@@ -36,6 +51,9 @@ export class DocumentRenderer extends BaseNodeRenderer<DocumentNode> {
     'localdatetime',
     'doctime',
     'docdatetime',
+    /doctype(-[-\w]+)?/,
+    /backend(-[-\w]+)?/,
+    /basebackend(-[-\w]+)?/,
   ];
 
   render(node: DocumentNode): string {
@@ -43,9 +61,13 @@ export class DocumentRenderer extends BaseNodeRenderer<DocumentNode> {
     const doctitle = attributes.doctitle;
     const nonDefaultAttributes = this.getNonDefaultAttributes(node);
     const children = node.getContent();
+    const id = node.getId();
+    // 作者这一行也可能是一句 include:: 指令，但是现在用的转换机制无法正确识别 include:: 指令，因此只能把它替换回去
+    const authors = restoreIncludeUrl(node.getAuthors().map(author => buildAuthorLine(author)).join('; '));
     const body = [
+      getLinesBeforeTitle(node, id),
       doctitle && `= ${doctitle}`,
-      node.getAuthors().map(author => buildAuthorLine(author)).join('; '),
+      authors,
       nonDefaultAttributes.map(it => renderAttribute(it.name, it.value)).join('\n'),
     ].filter(it => !!it).join('\n');
     return body + '\n\n' + children;
