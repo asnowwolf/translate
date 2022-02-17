f31f48a0b1075e79bb1f6970f0935cafe5ed11cd
Zhicheng WANG <zcwang@thoughtworks.com>
2022-02-17 19:32:51 +0800

    fix: 链接第一位总是放文字

--- src/utils/asciidoctor/adoc-converter/renderers/inline-anchor-renderer.ts
+++ src/utils/asciidoctor/adoc-converter/renderers/inline-anchor-renderer.ts
@@ -12,7 +12,8 @@ export class InlineAnchorRenderer extends InlineNodeRenderer<InlineNode> {
         } else {
           const nonDefaultAttributes = this.getNonDefaultAttributes(node)
             .filter(it => it.name !== 'window' || text.endsWith('^') && it.value === '_blank');
-          return `${target}[${this.renderAttributes(nonDefaultAttributes)}]`;
+          const content = [text, this.renderAttributes(nonDefaultAttributes)].filter(it => !!it).join(', ');
+          return `${target}[${content}]`;
         }
       case 'xref':
         const content = [target.replace(/^#/, '').replace(/^(.+?)(\.\w+)#(.*)$/, '$1#$3'), text].filter(it => !!it).join(', ');
