0593da1631557f47ccd6881d2f9de6b466a47b50
Zhicheng WANG <zcwang@thoughtworks.com>
2022-06-06 08:26:58 +0800

    fix: 比较时忽略大小写

--- src/translator/same-except-whitespace.ts
+++ src/translator/same-except-whitespace.ts
@@ -1,3 +1,3 @@
 export function sameExceptWhitespace(s1: string, s2: string): boolean {
-  return s1.replace(/\s/g, '') === s2.replace(/\s/g, '');
+  return s1.replace(/\s/g, '').toLowerCase() === s2.replace(/\s/g, '').toLowerCase();
 }
