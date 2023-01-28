9d6afa0641153aa4ef16cc7ab422c8cece3f3b65
Zhicheng WANG <zcwang@thoughtworks.com>
2023-01-28 16:06:53 +0800

    fix: 修改了 Google 引擎的结果替换方式

--- src/translation-engine/google-engine.spec.ts
+++ src/translation-engine/google-engine.spec.ts
@@ -3,11 +3,7 @@ import { GoogleTranslationEngine } from './google-engine';
 describe('goog engine', () => {
   it('should translate by google', async () => {
     const engine = new GoogleTranslationEngine();
-    engine.translate(`The implementation of \`Into<Vec<u8>>\` for \`String\` simply takes the
-\`String\`'s heap buffer and repurposes it, unchanged, as the returned
-vector's element buffer. The conversion has no need to allocate or
-copy the text. This is another case where moves enable efficient
-implementations.`, 'markdown').then(result => {
+    engine.translate(`\`abc\`! this is a word!`, 'markdown').then(result => {
       expect(result).toBe(`\`String\`的\`Into<Vec<u8>>\`的实现只是获取\`String\`的堆缓冲区并将其重新用作返回向量的元素缓冲区，保持不变。转换不需要分配或复制文本。这是移动实现高效实施的另一种情况。`);
     });
     await engine.flush();
