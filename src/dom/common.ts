c9f7468d8160fcbee3cc3012bd68629a3eae4c7a
Zhicheng WANG <zcwang@thoughtworks.com>
2023-01-31 21:42:16 +0800

    fix: 清理无用的代码

--- src/dom/common.ts
+++ src/dom/common.ts
@@ -1,23 +1,7 @@
-import { basename, extname, join } from 'path';
-import { existsSync, mkdirSync } from 'fs';
-import { homedir } from 'os';
-
 export function containsChinese(text?: string): boolean {
   text = text?.toString();
   if (!text) {
     return false;
   }
   return /\p{Script=Han}/gu.test(text);
-}
-
-export function basenameWithoutExt(filename = ''): string {
-  return basename(filename, extname(filename));
-}
-
-export function ensureHomeDir() {
-  const homeDir = join(homedir(), `.ng-translator`);
-  if (!existsSync(homeDir)) {
-    mkdirSync(homeDir);
-  }
-  return homeDir;
 }
