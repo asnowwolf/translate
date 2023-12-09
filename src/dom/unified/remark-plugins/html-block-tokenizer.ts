c0b1f110424e9f07c5bd5e3ce29c535f489789e6
Ralph Wang <traveller@163.com>
2023-12-09 10:56:49 +0800

    feat: 支持 Angular Doc 的 <div class="filetree"> 代码块

--- src/dom/unified/remark-plugins/html-block-tokenizer.ts
+++ src/dom/unified/remark-plugins/html-block-tokenizer.ts
@@ -2,7 +2,8 @@ import { Eater, UnifiedParser } from './unified-parser';
 import { Node } from 'unist';
 
 export function htmlBlockTokenizer(this: UnifiedParser, eat: Eater, value: string, silent?: boolean): Node | boolean | undefined {
-  const matches = /^<(code-example|code-examples|code-tabs)\b(.*?)>([\s\S]*?)<\/\1>/s.exec(value);
+  const matches = /^<(code-example|code-examples|code-tabs)\b(.*?)>([\s\S]*?)<\/\1>/s.exec(value) ??
+    /^( *)<div class="filetree">.*?\n\1<\/div>$/s.exec(value);
   if (matches) {
     try {
       if (silent || !matches) {
